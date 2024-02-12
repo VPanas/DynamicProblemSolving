7f58e1b4fb1d33e10667390fc7929a3293b8b246
Julian <40327956+Archi657@users.noreply.github.com>
2024-02-11 20:15:34 -0500

    feat: ✨ Styles | Nav bar | User profile
    This commit has add new styles, adding static file main.css, creating the user profile view, visualize user photo, username

--- src/front-end/main.py
+++ src/front-end/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, abort, render_template
 import os
+from datetime import date
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='static')
 
 problems_data = [
     {
@@ -24,44 +26,71 @@ problems_data = [
     },
 ]
 
+users = [
+    {
+        'id':1001,
+        'username':'jorge24',
+        'register_date':date(2002, 12, 31),
+        'photo_url':'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg',
+        'problems_history':[
+            {
+                'problem_id':100,
+                'date_solved':date(2024, 2, 9),
+                'score':90
+            },
+            {
+                'problem_id':101,
+                'date_solved':date(2024, 2, 10),
+                'score':60
+            },
+            {
+                'problem_id':102,
+                'date_solved':date(2024, 2, 11),
+                'score':20
+            }
+        ],
+
+    }
+]
+
 @app.route('/')
 def index():
     # get problems from the db
-    return render_template('index.html')
+    return render_template('index.html', user=users[0])
 
 @app.route('/problems')
 def problems():
     # query from db all problems
-    return render_template('problems/problems.html', problems = problems_data)
+    return render_template('problems/problems.html', problems = problems_data, user=users[0])
 
 @app.route('/problem/<int:id>')
 def problem(id):
     selected_problem = next((p for p in problems_data if p['id'] == id), None)
 
     if selected_problem:
-        return render_template('problems/problem.html', problem=selected_problem)
+        return render_template('problems/problem.html', problem=selected_problem, user=users[0])
     else:
         abort(404)
 
 @app.route('/about')
 def about():
     #about us info
-    return render_template('info/about.html')
+    return render_template('info/about.html', user=users[0])
 
 @app.route('/contact')
 def contact():
     #form and diff links to contact
-    return render_template('info/contact.html')
+    return render_template('info/contact.html', user=users[0])
 
 @app.route('/top')
 def top():
     #best results
-    return render_template('top/top.html')
+    return render_template('top/top.html', user=users[0])
 
-@app.route('/profile/<id>')
-def profile(id):
+@app.route('/profile/<username>')
+def profile(username):
     #el perfil
-    return render_template('profile/profile.html')
+    return render_template('profile/profile.html', user=users[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
